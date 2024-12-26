ef519ee5cd7ee6164747c5a1cbef72fddda973b7
LautaroFuente <lautaro.fuente@yahoo.com>
2024-12-25 22:02:23 -0300

    update user entity

--- finance-app/src/main/java/com/finance_app/finance_app/entities/User.java
+++ finance-app/src/main/java/com/finance_app/finance_app/entities/User.java
@@ -1,5 +1,7 @@
 package com.finance_app.finance_app.entities;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,12 +17,14 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private BigDecimal balance;
 	
 	public User(String name, String email, String password) {
 		super();
 		this.name = name;
 		this.email = email;
 		this.password = password;
+		this.balance = new BigDecimal("0");;
 	}
 
 	public User() {
@@ -54,5 +58,15 @@ public class User {
 	public Long getId() {
 		return id;
 	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+	
+	
 	
 }

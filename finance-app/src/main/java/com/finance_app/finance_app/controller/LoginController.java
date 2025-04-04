0a52aeb406aa5c1dcacc0472a02d4f4065cfebf0
LautaroFuente <lautaro.fuente@yahoo.com>
2025-04-04 13:54:23 -0300

    update login view and his style and refactoring login controller

--- finance-app/src/main/java/com/finance_app/finance_app/controller/LoginController.java
+++ finance-app/src/main/java/com/finance_app/finance_app/controller/LoginController.java
@@ -1,9 +1,6 @@
 package com.finance_app.finance_app.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import com.finance_app.finance_app.entities.User;
@@ -15,15 +12,12 @@ import com.finance_app.finance_app.validation.Validator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 @Controller
 public class LoginController {
@@ -32,13 +26,10 @@ public class LoginController {
 	private UserService userService;
 	
 	@Autowired
-	private LoadNewViewService goBackService;
+	private LoadNewViewService loadNewViewService;
 	
 	@Autowired
 	private AuthService authService;
-
-	@Autowired
-	private ApplicationContext context;
 	
     @FXML
     private TextField emailField;
@@ -71,6 +62,7 @@ public class LoginController {
 			return;
 		}
 		
+		cleanFields();
 		String responseLogin = this.authService.authUser(this.emailField.getText(), this.passwordField.getText());
 		
 		if("Usuario logueado".equals(responseLogin)) {
@@ -82,22 +74,10 @@ public class LoginController {
 				SessionManager.getInstance().login(user);
 				
 				// Cargar la vista
-				FXMLLoader loader = new FXMLLoader();
-	            loader.setLocation(getClass().getResource("/fxml/wallet-view.fxml"));
-	            loader.setControllerFactory(context::getBean);
-
-	            Parent root = loader.load();
-
-	            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();  // Usamos `event.getSource()` para obtener el origen del evento (el botón)
-	            
-	            double currentWidth = stage.getWidth();
-	            double currentHeight = stage.getHeight();
-
-	            // Crear la nueva escena y mostrarla
-	            Scene scene = new Scene(root, currentWidth, currentHeight);
-	            stage.setScene(scene);
-	            stage.show();
-	        } catch (IOException e) {
+				this.loadNewViewService.loadNewView(event, "/fxml/wallet-view.fxml");
+	        } catch (Exception e) {
+	        	Alert alert = new Alert(Alert.AlertType.ERROR, "Error al iniciar sesion del usuario, por intente nuevamente.", ButtonType.OK);
+	        	alert.showAndWait();
 	            e.printStackTrace();
 	        }
 			
@@ -118,6 +98,6 @@ public class LoginController {
 	}
 	
 	public void goBack(ActionEvent event) {
-		this.goBackService.loadNewView(event, "/fxml/home-view.fxml");
+		this.loadNewViewService.loadNewView(event, "/fxml/home-view.fxml");
 	}
 }

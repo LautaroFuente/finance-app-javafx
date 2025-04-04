0bb10728ea81f8cf03759f3fd415c77d4cb69c51
LautaroFuente <lautaro.fuente@yahoo.com>
2025-04-03 23:07:37 -0300

    checked changes in register controller and update wallet service and repository

--- finance-app/src/main/java/com/finance_app/finance_app/repository/WalletRepository.java
+++ finance-app/src/main/java/com/finance_app/finance_app/repository/WalletRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.finance_app.finance_app.entities.Wallet;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long>{
 	
 	@Query("SELECT w FROM Wallet w WHERE w.user.id = :userId")
